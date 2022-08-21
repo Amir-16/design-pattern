5e415b5265d698f0c8fb66dd4bad2f0f8174e6b4
Amir-16 <greenfitter516@gmail.com>
2022-08-21 18:12:06 +0600

    add 2 design pattern

---
+++ facade/facade.php
@@ -0,0 +1,41 @@
+<?php
+
+class Person
+{
+    public function getFullName()
+    {
+        return 'Amirul Islam';
+    }
+}
+
+class PersonFacade
+{
+    static $instance;
+
+    public static function __callStatic($method, $args)
+    {
+        if (null === static::$instance) {
+            static::$instance = new Person;
+        }
+
+        $instance = static::$instance;
+
+        switch (count($args)) {
+            case 0:
+                return $instance->$method();
+            case 1:
+                return $instance->$method($args[0]);
+            case 2:
+                return $instance->$method($args[0], $args[1]);
+            case 3:
+                return $instance->$method($args[0], $args[1], $args[2]);
+            case 4:
+                return $instance->$method($args[0], $args[1], $args[2], $args[3]);
+            default:
+                return call_user_func_array([$instance, $method], $args);
+        }
+
+    }
+}
+
+var_dump(PersonFacade::getFullName());
